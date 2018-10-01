a50309c39d66bd31703f7ac72e8ebf3e134ace02
Jiri M <mikulasekj85@gmail.com>
2018-10-02 00:09:20 +0200

    forex-currencies were added, exception were added

--- CONV/convertor.py
+++ CONV/convertor.py
@@ -2,6 +2,12 @@ from forex_python.converter import CurrencyRates
 from forex_python.converter import CurrencyCodes
 import json
 
+class WrongInputCurrencyError(Exception):
+    pass
+
+class WrongOutputCurrencyError(Exception):
+    pass
+
 class convertor:
 
     def __init__(self,input_currency,amount,output_currency=None):
@@ -11,11 +17,12 @@ class convertor:
         self.amount=amount
 
     # load the json file with currencies codes and symbols
-        with open('currencies.json',encoding="utf8") as f:
+        with open('forex_currencies.json',encoding="utf8") as f:
             data = json.load(f)
 
-        code=[x for x in data]
-        self.code_symbol_dict={x:data[x]['symbol'] for x in code}
+        # code=[x for x in data]
+        # self.code_symbol_dict={x:data[x]['symbol'] for x in code}
+        self.code_symbol_dict=data
 
     #create dictionary with key as a symbol and code as a value
         self.symbol_code_dict={key:value for value,key in self.code_symbol_dict.items()}
@@ -31,22 +38,39 @@ class convertor:
 
         if self.output_currency in self.currency_symbol_list:
             self.output_currency=self.symbol_code_dict[self.output_currency]
+
+    def check_inputs(self):
+        try:
+            if self.input_currency not in self.currency_code_list:
+                raise WrongInputCurrencyError
+            if self.output_currency not in self.currency_code_list:
+                raise WrongOutputCurrencyError
+            if not isinstance(self.amount ,float):
+                raise ValueError
+        except WrongInputCurrencyError:
+            print('Input currency is not supported' )
+            raise
+        except WrongOutputCurrencyError:
+            print('Output currency is not supported' )
+            raise
+        except ValueError:
+            print('Amount must be a number' )
+            raise
             
             
     def convert(self):
         
         c=CurrencyRates()
         self.convert_symbols()
+        self.check_inputs()
         output_dict={}     
 
-    # if the output value is not given the whole known currencies are shown
+    # if the output value is not given the whole known(by forex) currencies are shown
         if self.output_currency==None:
             for curr in self.currency_code_list:
-                try:
-                    converted_value=c.convert(self.input_currency,curr,self.amount)
-                except:
-                    continue
+                # if the currency code is not support by forex-python(forex raise the error) than skip the currency
                 
+                converted_value=c.convert(self.input_currency,curr,self.amount) 
                 output_dict.update({curr:converted_value})
 
         else:
---
+++ CONV/helper.py
@@ -0,0 +1,24 @@
+import json
+
+forex_dict={"EUR":1.0,"AUD":1.607,"BGN":1.9558,"BRL":4.6869,"CAD":1.4867,"CHF":1.1414,"CNY":7.972,"CZK":25.765,"DKK":7.4557,"GBP":0.89078,"HKD":9.0848,"HRK":7.433,"HUF":323.04,"IDR":17304.55,"ILS":4.2251,"INR":84.6215,"ISK":129.3,"JPY":132.25,"KRW":1290.16,"MXN":21.5632,"MYR":4.8049,"NOK":9.4465,"NZD":1.7572,"PHP":62.733,"PLN":4.2796,"RON":4.6605,"RUB":76.1229,"SEK":10.33,"SGD":1.5903,"THB":37.418,"TRY":6.908,"USD":1.1606,"ZAR":16.4326}
+forex_list=list(forex_dict.keys())
+
+
+with open('currencies.json',encoding="utf8") as f:
+    data = json.load(f)
+
+code=[x for x in data]
+code_symbol_dict={x:data[x]['symbol'] for x in code}
+print(len(code_symbol_dict))
+
+
+
+forex_dict={key:value for key,value in code_symbol_dict.items() if key in forex_list }
+
+print(forex_dict)
+print(len(forex_dict),len(forex_list))
+
+json_forex = json.dumps(forex_dict,indent=4)
+
+with open('forex_currencies.json','w',encoding="utf8") as f:
+    f.write(json_forex)
